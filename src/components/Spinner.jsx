fe577491154d0948b5af6e73ce37003a2d095959
theBappy <abappy575@gmail.com>
2025-01-07 10:25:25 +0600

    files are added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+      color="#433Bca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+
+export default Spinner;
